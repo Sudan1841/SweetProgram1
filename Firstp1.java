2f5033f6b824d32763db8264c794fe0eabde96c7
Abdulmajid Ramadan Hassan <45086070+Tigerner@users.noreply.github.com>
2021-06-27 15:18:51 -0600

    Add files via upload

---
+++ Firstp1.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Firstp1 {
+	//public is access modefier mean who can use
+	//class is contain everything has member
+
+	public static void main(String[] args) {
+		// public is access modifer mean who can use 
+		//void -
+		//static - no instance of class is needed 
+		//main - is method is to do something 
+		//arguments -is what is you pass to a method
+		//statement telling the computer to do something 
+		//properties is  store something 
+		//object- is instance of class
+		//parameter - is variable to store arguments (define)
+		//Scanner input = new Scanner();
+		//type identifer = new type();
+//		int i = 9;
+//		while(i>=0) {
+//			int k = i;
+//			while(k>=0) {
+//				System.out.println(k+ " ");
+//				k--;
+//			}
+//			System.out.println();
+//			i++;
+//		}
+		
+//		for(int i = 9; i>=0; i--) {
+//			for(int k=i; k<=0; k--) {
+//				System.out.println(k+ " ");
+//				
+//			}
+		String des = "1995";
+		 Scanner myObj = new Scanner(System.in);
+
+		    System.out.println("Enter name, age and salary:");
+
+		    // String input
+		    String name = myObj.nextLine();
+
+		    // Numerical input
+		    int age = myObj.nextInt();
+		    double salary = myObj.nextDouble();
+
+		    // Output input by user
+		    System.out.println("Name: " + name);
+		    System.out.println("Age: " + age);
+		    System.out.println("Salary: " + salary);
+		    
+		//int or double or float or short, long;
+//			
+//		}
+		////		System.out.println(1);
+		//		String description = "Java is one of most propural language that has been created by Microsystem by James Gosling";
+		//		 System.out.println("Enter your name");
+		//		    Scanner input = new Scanner(System.in);
+		//		    
+		//		    String Name = input.nextLine();
+		//		    System.out.println("Your name is "+ Name);
+		//		    System.out.println("Hello Java "+description);
+		//		    System.out.println("You were born 1995 ");
+		//		    System.out.println("Your have been create by James Gosling");
+		//		    System.out.println("You Are fun computer programming language");
+				    
+				    
+			}
+
+}
